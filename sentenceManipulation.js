8ee353fb6af2a0b7b8a4f55349fe0e9f14937302
Muhammad <muhammadrizkyputraaa@gmail.com>
2023-10-11 22:15:31 +0700

    test 6

---
+++ sentenceManipulation.js
@@ -0,0 +1,14 @@
+function sentenceManipulation (sentence){               
+    let arrSent = sentence.split(" ");                 
+    let arrNew = [];                                    
+
+    for (let i = 0; i < arrSent.length; i++) { 
+        if(arrSent[i].charAt(0).match(/[aiueoAIUEO]/)){
+            arrNew.push(arrSent[i]);
+        } else {
+            arrNew.push(arrSent[i].slice(1).concat(arrSent[i].charAt(0)).concat('nyo'));
+        }
+    }
+    console.log(arrNew.join(' '));
+}
+sentenceManipulation('ibu pergi ke pasar bersama aku');
