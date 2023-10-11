fa587b2a14c03cc04193ad7255c3eb7a925f5c67
Muhammad <muhammadrizkyputraaa@gmail.com>
2023-10-11 17:37:59 +0700

    test 5

---
+++ stringManipulation.js
@@ -0,0 +1,13 @@
+function stringManipulation(word){
+
+   if (word[0].match(/[aiueoAIUEO]/)) {
+    console.log(word);
+   } else {
+    let newWord = word.slice(1,word.length).concat(word[0]).concat('nyo');
+    console.log(newWord);
+   }
+}
+
+
+stringManipulation('ayam');
+stringManipulation('bebek');
