4b2c73d2cdae4d85c91527372094a15bfa6d8b32
Muhammad <muhammadrizkyputraaa@gmail.com>
2023-10-31 21:11:14 +0700

    c16

---
+++ Simple OOP/c16.js
@@ -0,0 +1,27 @@
+class Tyre {
+    constructor(brand, size) {
+
+    }
+}
+
+class Car {
+
+}
+
+class CarFactory {
+    constructor() {
+        this.car = []
+    }
+    produce(year) {
+
+    }
+    guaranteeSimulation(simulationYear) {
+
+    }
+}
+
+const toyota = new  CarFactory()
+toyota.produce(2020)
+toyota.produce(2022)
+toyota.result()
+toyota.guaranteeSimulation(2025)
